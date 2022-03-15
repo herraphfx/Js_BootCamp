8127f7d9a2a39f360bc67f914796f47a89596178
herraphfx <herraph@gmail.com>
2022-03-15 10:05:10 +0400

    Personal Portfolio New Web

---
+++ week_2_Javascript/intro_js_jav.js
@@ -0,0 +1,104 @@
+console.log(
+    "THis is JS"
+)
+
+
+let a = 2;
+let b = 5;
+let sum = a + b;
+console.log(sum);
+
+//undefined, variable that does not have a value
+let A;
+console.log(A);
+
+let str = "Today is a a beautiful day";
+let str3 = "hello"
+console.log(str);
+
+let str1 = "1"; 
+let str2 = "2"
+
+console.log(str1 + str2);
+
+//string methods
+
+console.log(str.indexOf('0'));
+console.log(str3.substring(1));
+console.log(str3.substring(
+    str3.indexOf('e'),
+    str3.indexOf('o') + 1
+))
+
+console.log(str3.replace('o', 'a'));
+
+
+//NUmber methods
+//toString()
+
+let num1 = 4;
+let num2 = 3;
+console.log(num1);
+console.log(num1.toString());
+
+//toFixed
+let pi = 3.14159266535;
+console.log(pi.toFixed(3));
+
+//Boolean
+let var1 =5;
+let var2 = "5";
+
+console.log(Boolean(var1==var2));
+console.log(Boolean(var1===var2));
+console.log(Boolean(var1 != var2));
+
+//exercise 6
+
+let ls1 = 5 + "34";
+
+console.log(ls1);
+
+let ls2 = 5 - "34";
+
+console.log(ls2);
+
+let ls3 = 10 %5;
+console.log(ls3);
+
+let ls4 = 5 % 10;
+console.log(ls4);
+
+let ls5 = "Java" + "Script";
+console.log(ls5);
+
+let ls6 = " " + " ";
+console.log(ls6);
+
+let ls7 = " " + 1;
+console.log(ls7);
+
+let ls8 = true + true;
+console.log(ls8);
+
+ls9 = true + false;
+console.log(ls9);
+
+let ls10 = false + true;
+console.log(ls10);
+
+let ls11 = false -true;
+console.log(ls11);
+
+
+console.log(!true);
+
+console.log(3-4);
+
+let ls12 = "Bob" + "bill"
+console.log(ls12);
+
+let ls13 = 6 + Number("three");
+console.log(ls13);
+
+//Exercise 5
---
+++ week_2_Javascript/js_exercise.js
@@ -0,0 +1,18 @@
+//exercise 1
+
+let addressNumber = "Palm Plaza Residence";
+let addressStreet = "Royal Road, Beau Bassin";
+let country = "Mauritius";
+
+globalAddress = "I live in "+ addressNumber + " "+ addressStreet + ", "+ country;
+
+
+console.log(globalAddress);
+
+//exercise 2
+let dob = 1992;
+let fdate = 2025;
+
+fyr = fdate - dob;
+
+console.log("I will be " + fyr + " in " + fdate);
