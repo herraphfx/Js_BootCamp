4641407a880d1c6fb01901b2be40ee3fca7b7163
herraphfx <herraph@gmail.com>
2022-04-12 12:18:51 +0400

    SQL daily challenge

---
+++ Week_6_Database/Exercise XP Ninja/ninja_students.sql
@@ -0,0 +1,37 @@
+-- set datestyle = 'ISO, DMY'
+
+-- SELECT * FROM students
+
+-- select first_name,last_name from students
+
+-- select first_name,last_name from students where id =2;
+
+-- select first_name,last_name from students where last_name = 'Benichou' AND first_name = 'Marc'
+
+-- select first_name,last_name from students where last_name = 'Benichou' OR first_name = 'Marc'
+
+-- select first_name, last_name from students where first_name 
+
+-- select first_name, last_name from students where first_name ilike '%a%'
+
+-- select first_name, last_name from students where first_name ilike 'a%'
+
+-- select first_name, last_name from students where first_name ilike '%a'
+
+-- select first_name, last_name from students where first_name ilike '%a_'
+
+-- select first_name,last_name from students where id=1  OR  id=3;
+
+-- select * from students where birth_date > '1/01/2000' 
+
+-- Exercise XP GOLD
+
+-- select first_name, last_name, birth_date from students where id <= 4 order by last_name asc;
+
+-- select * from students order by birth_date DESC limit 1
+
+-- select first_name, last_name , birth_date from students offset 2 limit 3;
+
+
+
+
---
+++ Week_6_Database/daily_challenge/daily.sql
@@ -0,0 +1,3 @@
+insert into actors_2(firstname, lastname, age, oscars)
+values('Henry', 'Oko', '1992/4/4', '3')
+
