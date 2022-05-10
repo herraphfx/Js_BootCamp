740ec1838c70a1842a4e350d229006e805d81844
herraphfx <herraph@gmail.com>
2022-05-10 15:58:56 +0400

    changes on fetch

--- React_native/js-ft-app/App.js
+++ React_native/js-ft-app/App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This Is My website</Text>
-      <StatusBar style="auto" />
-    </View>
+          <NavigationContainer>
+              <View style={styles.container}>
+                <Text>This Is My website</Text>
+                <StatusBar style="auto" />
+              </View>
+         </NavigationContainer>
+    
   );
 }
 
--- Week_10_project/server.js
+++ Week_10_project/server.js
@@ -1,4 +1,11 @@
+import express from 'express';
+const app = express();
+import fetch from 'node-fetch';
+const PORT = process.env.PORT || 5001;
 
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 let url = "http://api.openweathermap.org/data/2.5/forecast?lat=32.109333&lon=34.855499&appid=bf8bdc6ff3f8af3bd4bad0fa482bfc3c";
 
@@ -12,57 +19,20 @@ fetch(url)
     console.log(response);
 })
 
-
-async function get_city(){
-    let city = await fetch(url)
-  
+const get_city = async () =>{
+    let city = await fetch(url);
     let data = await city.json();
-    console.log(data);
-   return data
+    return data
 }
 
-let new_city = get_city();
+app.get('/', function(req, res){
+    get_city().then((data)=>{
+       res.render('index', {title: 'Weather App', weatherData: data});
+    })
+   
+})
 
-console.log(new_city)
-
-
-
-
-
-
-
-// let xhr = new XMLHttpRequest();
-
-// console.log(xhr.readyState); 
-// // xhr.open(method, URL, [async, user, password])
-
-// let url = 'http://dataservice.accuweather.com/currentconditions/v1/topcities/50?apikey=1sHRW57gDYemvJxLAx4O5gMabqJbAGDZ'
-
-// xhr.open('GET', url, {mode: "no-cors"});
-
-// console.log(xhr.readyState); //1
-
-// //Add headers
-
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.setRequestHeader('Access-Control-Allow-Origin', "*");
-// xhr.setRequestHeader('Access-Control-Allow-Headers', "*");
-// xhr.withCredentials = false;
-// //send request
-// xhr.send();
-// console.log(xhr.readyState); 
-
-// xhr.onload = function() {
-//     if(xhr.status === 200) {
-//         console.log('OK');
-//         console.log(JSON.parse(xhr.response));
-//     }
-//     else {
-//         console.log('Error');
-//         alert(`Status code: ${xhr.status} - ${xhr.statusText}`);
-//     }
-//     console.log(xhr.readyState); 
-// }
-
-// //Fetching data
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+})
 
