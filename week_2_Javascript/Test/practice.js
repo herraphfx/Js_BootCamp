d8dc48297b00dade96d29fe7eef93353d5e2c13f
herraphfx <herraph@gmail.com>
2022-06-25 16:05:24 +0400

    k sorted

--- week_2_Javascript/Test/practice.js
+++ week_2_Javascript/Test/practice.js
@@ -930,10 +930,44 @@ function medianOfArray(array){
         console.log((array[array.length/2] + array[array.length/2 - 1]) / 2)
     }
 }
-array = [1,2,3,4,5,6]
-medianOfArray(array)
+// array = [1,2,3,4,5,6]
+// medianOfArray(array)
 
+function medianOfTwoSortedArray(arr1, arr2, pos){
+    if(pos <= 0){
+        return -1
+    }
 
+    if(pos ==1){
+        return (arr1[0] + arr2[0])/2
+    }
+
+    if(pos == 2){
+        return (Math.max(arr1[0]. arr2[0]) + Math.min(arr1[1] + arr2[1])) / 2
+    }
+
+    let median1 = medianOfArray(arr1);
+    let median2 = medianOfArray(arr2);
+
+    if(median1 == median2){
+        return median1;
+    }
+
+    let evenOffset = pos % 2 == 0 ? 1 : 0;
+    let offsetMinus = Math.floor(pos/2) - evenOffset
+    let offsetPlus = Math.floor(pos/2) + evenOffset
+
+    if(median1 < median2){
+        return medianOfTwoSortedArray(arr1.slice(offsetMinus),
+        arr2.slice(offsetMinus), offsetPlus)
+    }
+    else{
+        return medianOfTwoSortedArray(arr2.slice(offsetMinus),
+        arr1.slice(offsetMinus), offsetPlus)
+    }
+}
+
+medianOfTwoSortedArray([1 , 2 , 3 ], [4 , 5 , 6 ], 3 ); 
 
 
 
